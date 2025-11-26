77f013362e9205a398500b3f3b5069a4f643d201
James Jensen <jamesarthurjensen@gmail.com>
2025-11-26 15:48:41 +0200

    Write layer

---
+++ packages.el
@@ -0,0 +1,8 @@
+(defconst markdown-agda-packages
+  '(
+     (markdown-agda-mode :location (recipe
+                                     :fetcher github
+                                     :repo lclem/markdown-agda-mode))
+     (polymode :location elpa)
+     (poly-markdown :location elpa)
+     ))
